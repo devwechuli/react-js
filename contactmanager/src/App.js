985a6c2a2cd6df3ead3bf7203c15205c62c3cafd
Wechuli <wechulipaul@yahoo.com>
2018-10-10 12:28:04 +0300

    building functional and class components and adding state

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
-      <h1>The App Component</h1>
+
+        <Header branding="Contact Manager" />
+        <div className="container">
+
+          <Contact name="John Doe" email="jdoe@gmail.com"
+            phone="044338999" />
+          <Contact name="Karen Smith" email="karen@gmail.com"
+            phone="044338232999" />
+        </div>
+
       </div>
     );
   }
