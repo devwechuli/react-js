cc90c93700d25d3c3c47168171ce4fc86d015d96
Wechuli <wechulipaul@yahoo.com>
2018-10-17 23:20:33 +0300

    dealing with state and setstate

--- newapp/src/App.js
+++ newapp/src/App.js
@@ -8,7 +8,18 @@ class App extends Component {
       { id: 1, name: "Paul", age: 25 },
       { id: 2, name: "June", age: 22 },
       { id: 3, name: "Mega", age: 140 }
-    ]
+    ],
+    otherState:"this is another state"
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { id: 1, name: "Paul Wechuli", age: 25 },
+        { id: 2, name: "Juny", age: 28 },
+        { id: 3, name: "Mega", age: 1900 }
+      ]
+    });
   };
 
   render() {
@@ -16,7 +27,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, am a React App</h1>
         <p>This is actually working</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
