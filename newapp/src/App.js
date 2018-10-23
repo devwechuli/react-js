87484dbdbecd4aa1a38874400a875ce3b75f3405
wechuli <wechulipaul@yahoo.com>
2018-10-23 14:19:48 +0300

    conditionals and outputting lists

--- newapp/src/App.js
+++ newapp/src/App.js
@@ -23,15 +23,28 @@ class App extends Component {
     });
   };
 
-  nameChangedHandler = ( event ) => {
-    this.setState( {
-      persons: [
-        { name: 'Paul', age: 26 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 29 }
-      ]
-    } )
-  }
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  };
+  deletePersonHandler = personIndex => {
+    //const persons = this.state.persons.slice(); //Use slice without parameters to copy the whole array/object
+    //or we can use the spreas operator as below, this updates the state in an immutable fashion
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
 
   toggleContent = () => {
     const doesShow = this.state.showPersons;
@@ -39,33 +52,31 @@ class App extends Component {
   };
 
   render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
     return (
       <div className="App">
         <h1>Hi, am a React App</h1>
         <p>This is actually working</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
         <button onClick={this.toggleContent}>Toggle Content</button>
-
-        {this.state.showPersons ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, "Wechulip")}
-              changed={this.nameChangedHandler}
-            >
-              My Hobbies: Racing
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
   }
