ed60339551c57b99d6253869e4b89a8ee5326c40
Wechuli <wechulipaul@yahoo.com>
2018-11-10 00:02:41 +0300

    implementing lifecycle hooks

--- udemytest/src/components/Persons/Persons.js
+++ udemytest/src/components/Persons/Persons.js
@@ -1,17 +1,34 @@
-import React from "react";
-import Person from "./Person/Person";
 
-const Persons = props =>
-  props.persons.map((person, index) => {
-    return (
-      <Person
-        key={person.id}
-        name={person.name}
-        age={person.age}
-        click={() => props.clicked(index)}
-        changed={event => props.changed(event, person.id)}
-      />
-    );
-  });
+import Person from "./Person/Person";
+import React, { Component } from "react";
+
+// const Persons = props =>
+//   props.persons.map((person, index) => {
+//     return (
+//       <Person
+//         key={person.id}
+//         name={person.name}
+//         age={person.age}
+//         click={() => props.clicked(index)}
+//         changed={event => props.changed(event, person.id)}
+//       />
+//     );
+//   });
+
+class Persons extends Component {
+  render() {
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          key={person.id}
+          name={person.name}
+          age={person.age}
+          click={() => this.props.clicked(index)}
+          changed={event => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  }
+}
 
 export default Persons;
