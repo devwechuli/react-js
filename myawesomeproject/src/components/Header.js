ea5a29aab2b05b8287718781b983155c4a0406fa
Wechuli <wechulipaul@yahoo.com>
2018-12-01 20:11:19 +0300

    revisiting classes

--- myawesomeproject/src/components/Header.js
+++ myawesomeproject/src/components/Header.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 
+
+
 class Header extends Component {
+  inputChangeHandler(e) {
+    console.log(e.target.value);
+    console.log("Hey");
+  }
   render() {
     return (
       <header>
         <div className="logo">Logo</div>
         <div className="input">
-          <input type="text" />
+          <input onChange={e => this.inputChangeHandler(e)} type="text" />
         </div>
       </header>
     );
---
+++ myawesomeproject/src/components/Payground.js
@@ -0,0 +1,22 @@
+class Car {
+  constructor() {
+    this.name = "Ford";
+    this.type = "Hatchback";
+    this.wheels = 4;
+  }
+  alertBrand() {
+    console.log(this.name);
+  }
+}
+
+class BMW extends Car {
+  constructor() {
+    super(Car);
+    this.windows = 5;
+  }
+}
+
+const car = new Car();
+const ford = new BMW();
+car.alertBrand();
+ford.alertBrand();
