408dcf11602499dbdafecc7d221a883c61fa4e0c
Wechuli <wechulipaul@yahoo.com>
2018-12-21 21:18:25 +0300

    redux-addition

---
+++ redux-tutorial/src/components/Posts.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import PostForm from "./PostForm";
+import Logo from './load.svg'
+
+class Posts extends Component {
+  state = {
+    posts: []
+  };
+  async componentDidMount() {
+    const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const jsonData = posts.json();
+    jsonData
+      .then(data => {
+        this.setState({ posts: data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const postItems = this.state.posts.slice().map(post => {
+      return (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      );
+    });
+    return (
+      <div>
+          <PostForm/>
+        <h1>Posts</h1>
+        {this.state.posts.length >0 ?postItems:(<div><img src={Logo} alt="spinner"/></div>)}
+      </div>
+    );
+  }
+}
+
+export default Posts;
