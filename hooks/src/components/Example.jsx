304f28b64389536a68f31a84cef28cf6b08530b8
wechuli <wechulipaul@yahoo.com>
2019-01-22 17:55:25 +0300

    useEffect hook

--- hooks/src/components/Example.jsx
+++ hooks/src/components/Example.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Example = () => {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <React.Fragment>
