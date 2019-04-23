2d02261acf94447c202d1a62a625c06f3958251d
wechuli <wechulipaul@yahoo.com>
2019-04-23 18:22:36 +0300

    using useState

--- back-to-basics/src/App.jsx
+++ back-to-basics/src/App.jsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [list, alterlist] = useState([
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="container">
+        <h2 className="has-text-centered subtitle">Books</h2>
+        <div className="columns">
+          {list.map(item => {
+            return (
+              <div className="column box m-r-md" key={item.objectID}>
+                <span className="has-text-weight-bold">
+                  {" "}
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <p>{item.author}</p>
+                <p>{item.num_comments}</p>
+                <p>{item.points}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
