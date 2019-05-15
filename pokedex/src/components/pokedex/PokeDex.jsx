a11f16b9d95410ecedbb6ac7cb9fa416b1308445
wechuli <wechulipaul@yahoo.com>
2019-05-15 18:21:25 +0300

    new pokedex

--- pokedex/src/components/pokedex/PokeDex.jsx
+++ pokedex/src/components/pokedex/PokeDex.jsx
@@ -11,10 +11,20 @@ let pokeDexData = [
   { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
   { id: 133, name: "Eevee", type: "normal", base_experience: 651 }
 ];
+
+let loosers_score = 0;
 const loosers = pokeDexData.filter(item => {
+if(item.base_experience < 100){
+  loosers_score += item.base_experience
+}
   return item.base_experience < 100;
 });
+
+let winners_score=0
 const winners = pokeDexData.filter(item => {
+ if(item.base_experience > 100){
+   winners_score += item.base_experience;
+ }
   return item.base_experience >= 100;
 });
 const PokeDex = () => {
@@ -25,6 +35,7 @@ const PokeDex = () => {
       <h2 className="subtitle has-text-danger has-text-centered m-t-md">
         Losing Hand
       </h2>
+      <p className="has-text-centered"><span className="has-text-weight-bold">Aggregate Score: </span>{loosers_score}</p>
       <div className="columns is-multiline m-b-md">
         {loosers.map(item => {
           return (
@@ -38,6 +49,7 @@ const PokeDex = () => {
       <h2 className="subtitle has-text-success has-text-centered m-t-md">
         Winning Hand
       </h2>
+      <p className="has-text-centered"><span className="has-text-weight-bold">Aggregate Score: </span>{winners_score}</p>
       <div className="columns is-multiline m-b-md">
         {winners.map(item => {
           return (
