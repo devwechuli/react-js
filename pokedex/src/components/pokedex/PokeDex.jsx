acc39b2bee3aeadf1edd5ca78d00558966be9d4b
wechuli <wechulipaul@yahoo.com>
2019-05-15 18:04:08 +0300

    filtering and state agiain

--- pokedex/src/components/pokedex/PokeDex.jsx
+++ pokedex/src/components/pokedex/PokeDex.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PokeCard from "./PokeCard/PokeCard";
 
-
 let pokeDexData = [
   { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
   { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
@@ -12,13 +11,35 @@ let pokeDexData = [
   { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
   { id: 133, name: "Eevee", type: "normal", base_experience: 651 }
 ];
-
+const loosers = pokeDexData.filter(item => {
+  return item.base_experience < 100;
+});
+const winners = pokeDexData.filter(item => {
+  return item.base_experience >= 100;
+});
 const PokeDex = () => {
   return (
     <div className="container">
-      <h2 className="title has-text-centered m-t-md">Pokedex</h2>
-      <div className="columns is-multiline m-t-md m-b-md">
-        {pokeDexData.map(item => {
+      <h2 className="title has-text-centered m-t-md m-b-md">Pokedex</h2>
+
+      <h2 className="subtitle has-text-danger has-text-centered m-t-md">
+        Losing Hand
+      </h2>
+      <div className="columns is-multiline m-b-md">
+        {loosers.map(item => {
+          return (
+            <div key={item.id} className={`column is-3`}>
+              <PokeCard poke={item} />
+            </div>
+          );
+        })}
+      </div>
+
+      <h2 className="subtitle has-text-success has-text-centered m-t-md">
+        Winning Hand
+      </h2>
+      <div className="columns is-multiline m-b-md">
+        {winners.map(item => {
           return (
             <div key={item.id} className={`column is-3`}>
               <PokeCard poke={item} />
