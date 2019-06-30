b57b1f1bd67d85721698f43fadf57b6d71304bb2
Wechuli <wechulipaul@yahoo.com>
2019-06-30 17:29:49 +0300

    lottery state exercise

--- lotterballs/src/App.jsx
+++ lotterballs/src/App.jsx
@@ -1,12 +1,13 @@
-import React,{Fragment} from 'react';
-import AllBalls from './components/AllBalls';
-import './components/ball.css'
+import React, { Fragment } from "react";
+import AllBalls from "./components/AllBalls";
+import "./components/ball.css";
 
 function App() {
   return (
-<Fragment>
-  <AllBalls/>
-</Fragment>
+    <Fragment>
+      <AllBalls />
+      <AllBalls title="Mini Daily" maxNum={10} maxBalls={4} />
+    </Fragment>
   );
 }
 
--- lotterballs/src/components/AllBalls.jsx
+++ lotterballs/src/components/AllBalls.jsx
@@ -1,20 +1,45 @@
-import React,{Component,Fragment} from 'react'
-import Ball from './Ball';
+import React, { Component, Fragment } from "react";
+import Ball from "./Ball";
 
+class AllBalls extends Component {
+  static defaultProps = {
+    title: "Lotto",
+    maxBalls: 6,
+    maxNum: 40
+  };
+  state = {
+    nums: Array.from({ length: this.props.maxBalls })
+  };
+  generate = () => {
+    // this.setState(curState => {
+    //   nums: curState.nums.map(n => {
+    //     return Math.floor(Math.random() * this.props.maxNum) + 1;
+    //   });
+    // });
+    const newArray = this.state.nums.map(n => {
+      return Math.floor(Math.random() * this.props.maxNum) + 1;
+    });
+    this.setState({ nums: newArray });
+  };
 
+  handleClick = () => {
+    this.generate();
+  };
 
-class AllBalls extends Component{
-static defaultProps
-    
-    render(){
-    
-        return(
-            <Fragment>
-              <Ball num={2}/>
-              <Ball num={3}/>
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <section>
+        <h1>{this.props.title}</h1>
+
+        <div>
+          {this.state.nums.map(n => {
+            return <Ball num={n} />;
+          })}
+        </div>
+        <button onClick={this.handleClick}>Generate </button>
+      </section>
+    );
+  }
 }
 
 export default AllBalls;
