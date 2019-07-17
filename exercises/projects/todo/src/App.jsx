f6b5be9a0a316649155523747b69de5a1f482ac0
Wechuli <wechulipaul@yahoo.com>
2019-07-17 17:54:11 +0300

    to do list project

---
+++ exercises/projects/todo/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function App() {
+  return (
+    <div className="container has-background-primary">
+      <p>Hi there, this is some react code</p>
+    </div>
+  );
+}
+
+export default App;
